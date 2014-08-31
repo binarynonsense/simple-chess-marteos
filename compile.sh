ca7d4c4e44866684b2b2b8ab76297c09e8459c3c
Alvaro Garcia <alvaro@binarynonsense.com>
2014-08-31 21:44:34 +0200

    first version

---
+++ compile.sh
@@ -0,0 +1,3 @@
+mgcc main.c draw.c fonts.c -o mprogram -Wall -I/home/alvaro/bin/marte/arch/drivers/svga -lsvga -lm
+#mgcc main.c -o mprogram -Wall -I/home/alvaro/bin/marte/arch/drivers/svga -lsvga -lm
+
